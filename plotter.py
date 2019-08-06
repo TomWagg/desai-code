4d47e1f1a8fa9f6641ea3c82adaad9005f723077
TomWagg <twagg@college.harvard.edu>
2019-08-06 15:32:51 -0400

    plotting styles

--- plotter.py
+++ plotter.py
@@ -1,6 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+titles = {
+    "absfitness": "Fixation for varying absolute fitnesses",
+    "migration": "Fixation for varying numbers of migrants"
+}
+
+labels = {
+    "absfitness": r"$s$ = ",
+    "migration": r"$m$ = "
+}
+
 def main():
     kinds = ["absfitness", "migration"]
     values = [[0.1, 0.5, 1, 2, 10], [1, 2, 10, 20]]
@@ -11,10 +21,14 @@ def main():
             demes, probs = [], []
             for result in sorted(results):
                 demes.append(result[0])
-                probs.append(result[2])
-            plt.plot(demes, probs, label=values[i][j])
-        plt.title(kinds[i])
+                probs.append(result[2])     
+            plt.plot(demes, probs, label=labels[kinds[i]] + str(values[i][j]))
+        plt.title(titles[kinds[i]])
         plt.legend(loc="best")
+        plt.xlabel(r"$M$, Number of Demes")
+        plt.grid(b=True, which="both")
+        plt.ylim((0, 1))
+        plt.ylabel("Fixation Probability")
         plt.show()
 
 if __name__ == "__main__":
